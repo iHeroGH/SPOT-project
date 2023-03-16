d9ae8b6e08120a6721833f5cc46e2250fe640aa8
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-03-15 21:43:32 -0700

    Change all xml paths

--- fetch/pre-processing/change_xmlpath.py
+++ fetch/pre-processing/change_xmlpath.py
@@ -5,15 +5,23 @@ import xml.etree.ElementTree as ET
 #new_path = r'C:\Users\jojom\OneDrive\Desktop\School\SPOT_project\fetch\can\images'
 
 #new_path = r'C:\Users\jojom\OneDrive\Desktop\School\SPOT_project\fetch\trashcan\images'
-new_path = r'C:\Users\jojom\OneDrive\Desktop\School\SPOT_project\fetch\trashcan_open\images'
+
+
+# The type of image
+img_dir = 'trashcan'
+
+# The camera mode
+cam_mode = 'back_cam'
+
+# The created annotations directory
+img_annotation_dir = "../" + img_dir + '/annotations/xml/' + cam_mode
+
+# The path to set all xml files' images to
+new_path = r'C:\Users\jojom\OneDrive\Desktop\School\SPOT_project\fetch' + f"\\{img_dir}\\images\\{cam_mode}"
 
 # Loop through all the XML files in the directory
 # Loop through all the directories and files within the annotations folder
-print(new_path)
-
-img_dir = 'trashcan_open'  # <- change as needed
-img_annotation_dir = "fetch/" + img_dir + '/annotations/xml'
-
+print(img_annotation_dir, new_path)
 for dirpath, dirnames, filenames in os.walk(img_annotation_dir):
     for filename in filenames:
         if not filename.endswith('.xml'):
